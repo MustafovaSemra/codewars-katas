161ae9598bd3339be043be9519d126ee9e4c9d43
MustafovaSemra <mustafovasemra@yahoo.com>
2021-08-10 18:42:15 -0400

    katas

--- 6 kyu/IQ Test/index.js
+++ 6 kyu/IQ Test/index.js
@@ -9,3 +9,32 @@ function iqTest(numbers) {
     return numArray.indexOf(testOdd[0]) + 1;
   }
 }
+
+function iqTest(numbers) {
+  numbers = numbers.split(" ");
+
+  let evens = [];
+  let odds = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] & 1) {
+      odds.push(i + 1);
+    } else {
+      evens.push(i + 1);
+    }
+  }
+
+  return evens.length === 1 ? evens[0] : odds[0];
+}
+
+function iqTest(numbers) {
+  // ...
+  return (
+    1 +
+    numbers
+      .split(" ")
+      .findIndex(
+        (x, idx, arr) => arr.filter((y) => y % 2 === x % 2).length === 1
+      )
+  );
+}
