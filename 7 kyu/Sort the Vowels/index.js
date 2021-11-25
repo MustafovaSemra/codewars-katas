c562d10fe2fb49c6bb6aa65a3d264275e23ccdf0
MustafovaSemra <mustafovasemra@yahoo.com>
2021-11-25 12:12:54 -0500

    katas

---
+++ 7 kyu/Sort the Vowels/index.js
@@ -0,0 +1,8 @@
+function sortVowels(s) {
+  return typeof s === "string"
+    ? s
+        .split("")
+        .map((el) => (/[aeuio]/gi.test(el) ? `|${el}` : `${el}|`))
+        .join("\n")
+    : "";
+}
